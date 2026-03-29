dc415918cfa5c69660f0fbe04704fad71ef2e127
AmitElad10 <amitelad2010@gmail.com>
2026-03-29 22:18:12 +0300

    finishing the xml

--- app/src/main/java/com/example/ex0611/MainActivity.java
+++ app/src/main/java/com/example/ex0611/MainActivity.java
@@ -1,18 +1,33 @@
 package com.example.ex0611;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private RadioButton rg1, rg2,rg3,rg4;
+    private Switch sW;
+    private RadioGroup radioGroup;
+    private ConstraintLayout main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clicked(View view) {
+
     }
 }
